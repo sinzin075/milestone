36f48b9732a7d96fa5b6171c26cf063655173976
sinzin075 <shoki@DESKTOP-DP52Q7G.>
2025-06-08 21:22:34 +0900

    feat: connect frontend to /users backend and add WebConfig for CORS

--- backend/src/main/java/com/calendar/milestone/controller/dto/request/UserRequest.java
+++ backend/src/main/java/com/calendar/milestone/controller/dto/request/UserRequest.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+
 public class UserRequest {
     @JsonProperty("id")
     private Integer id;
