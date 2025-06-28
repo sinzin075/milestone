988662aefd48fe3ce2ad1bd20abf3769a437e7bf
sinzin075 <shoki@DESKTOP-DP52Q7G.>
2025-06-28 21:33:00 +0900

    feat: add ApiStatus enum for common API response handling

---
+++ backend/src/main/java/com/calendar/milestone/controller/dto/response/common/ApiStatus.java
@@ -0,0 +1,29 @@
+package com.calendar.milestone.controller.dto.response.common;
+
+public enum ApiStatus {
+    OK(200,"Ok"),
+    CREATE(201,"Create"),
+    NO_CONTENT(204,"No Content"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    CONFLICT(409, "Conflict"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+    
+
+    final private int status;
+    final private String message;
+
+    ApiStatus(final int status,final String message){
+        this.status=status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+}
